aecadd0a77888e8fb096d78a3e406649e8ac00bb
Alex Barry <alex.barry@gmail.com>
2020-08-09 17:49:07 -0400

    Start to derive app buttons from custom base button class

--- lib/components/Buttons.dart
+++ lib/components/Buttons.dart
@@ -1,21 +1,56 @@
 import 'package:flutter/material.dart';
 
+class CustomButton extends StatelessWidget {
+  final List<Widget> children;
+  final void Function() onPressed;
+  final IconSide iconSide;
+  final MaterialTapTargetSize materialTapTargetSize;
+  final Color color;
+  final Color textColor;
+
+  CustomButton({
+    this.children,
+    this.onPressed,
+    this.color,
+    this.textColor,
+    this.iconSide = IconSide.Left,
+    this.materialTapTargetSize = MaterialTapTargetSize.shrinkWrap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      color: color,
+      textColor: textColor,
+      materialTapTargetSize: materialTapTargetSize,
+      onPressed: onPressed,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: children,
+      ),
+    );
+  }
+}
+
 class MaterialTextButton extends StatelessWidget {
   final Widget child;
   final void Function() onPressed;
   final Alignment alignment;
+  final MaterialTapTargetSize materialTapTargetSize;
 
   MaterialTextButton({
     this.child,
     this.onPressed,
     this.alignment = Alignment.center,
+    this.materialTapTargetSize = MaterialTapTargetSize.shrinkWrap,
   });
 
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
         visualDensity: VisualDensity.compact,
-        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        materialTapTargetSize: materialTapTargetSize,
         child: Align(
           alignment: Alignment.centerLeft,
           child: child,
@@ -35,30 +70,31 @@ class MaterialRaisedTextIconButton extends StatelessWidget {
   final Widget icon;
   final void Function() onPressed;
   final IconSide iconSide;
+  final MaterialTapTargetSize materialTapTargetSize;
+  final Color color;
+  final Color textColor;
 
   MaterialRaisedTextIconButton({
     this.label,
     this.icon,
     this.onPressed,
+    this.color,
+    this.textColor,
     this.iconSide = IconSide.Left,
+    this.materialTapTargetSize = MaterialTapTargetSize.shrinkWrap,
   });
 
   @override
   Widget build(BuildContext context) {
-    var left = this.iconSide == IconSide.Left
-        ? icon
-        : label;
+    var children = this.iconSide == IconSide.Left
+        ? [icon, label]
+        : [label, icon];
 
-    var right = this.iconSide == IconSide.Left
-        ? label 
-        : icon;
-
-    return RaisedButton.icon(
-        // visualDensity: VisualDensity.compact,
-        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-        label: right,
-        icon: left,
-        onPressed: onPressed,
-      );
+    return CustomButton(
+      color: color,
+      textColor: textColor,
+      onPressed: onPressed,
+      children: children,
+    );
   }
 }
