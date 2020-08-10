9f14184aeb9063bf06a7f72424ff1d57d2ebed51
Alex Barry <alex.barry@gmail.com>
2020-08-09 20:52:53 -0400

    More tinkering with the verse formatter

--- lib/services/VerseFormatter.dart
+++ lib/services/VerseFormatter.dart
@@ -1,18 +1,58 @@
+import 'package:tuple/tuple.dart';
+
 import './Bible.dart';
+
+enum TextNodeType {
+  VerseAnchor,
+  Text,
+}
 
 class TextNode {
   final int verse;
   final String text;
+  final TextNodeType textNodeType;
 
-  TextNode({ this.verse, this.text });
+  TextNode({
+    this.verse,
+    this.text,
+    this.textNodeType = TextNodeType.Text,
+  });
 }
 
 class Paragraph {
   List<TextNode> nodes = List<TextNode>();
   bool startWithIndent = true;
-}
 
-List<TextNode> toTextNodes(String text) {
+  Tuple2<Paragraph, int> appendFrom(Verse verse, { int offset = 0 }) {
+    var initialText = _cleanVerseText(verse);
+    var end = initialText.indexOf('</pb>', offset + 1);
+    var text = end == -1
+        ? initialText.substring(offset)
+        : initialText.substring(offset, end);
+
+    if (offset == 0) {
+      nodes.add(TextNode(
+        verse: verse.verse,
+        text: verse.verse.toString(),
+        textNodeType: TextNodeType.VerseAnchor,
+      ));
+    }
+
+    nodes.add(TextNode(
+      verse: verse.verse,
+      text: text,
+    ));
+
+    return end == -1
+      ? Tuple2<Paragraph, int>(this, 0)
+      : Tuple2<Paragraph, int>(Paragraph(), end + 1);
+  }
+
+  String _cleanVerseText(Verse verse) {
+    return verse.text
+        .replaceAll(new RegExp(r'<S>\d+</S>'), '')
+        .replaceAll(new RegExp(r'</?%s>'), '');
+  }
 }
 
 List<Paragraph> toParagraphs(List<Verse> verses) {
